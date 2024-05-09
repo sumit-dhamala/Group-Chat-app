32d50a14eed2fefab664a948460e1354d6f21de9
Bikal Chudal <bikalchudal11@gmail.com>
2024-05-09 11:07:59 +0545

    done

--- lib/resources/components/msg_container.dart
+++ lib/resources/components/msg_container.dart
@@ -12,25 +12,73 @@ class MsgContainer extends StatelessWidget {
         Expanded(
           child: Container(),
         ),
-        Container(
-          alignment: Alignment.centerRight,
-          margin: EdgeInsets.symmetric(vertical: 10),
-          // height: 50,
-          // width: MediaQuery.of(context).size.width * .6,
-          decoration: BoxDecoration(
-            color: pColor,
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                textAlign: TextAlign.end,
-                msg,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
+        InkWell(
+          onDoubleTap: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return Container(
+                    height: 100,
+                    width: 300,
+                    child: AlertDialog(
+                      title: Text(
+                        "Edit message",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      content: TextField(),
+                      actions: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStatePropertyAll(sColor),
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(pColor)),
+                              onPressed: () {},
+                              child: Text("Cancel"),
+                            ),
+                            TextButton(
+                              style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStatePropertyAll(sColor),
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(pColor)),
+                              onPressed: () {},
+                              child: Text("Edit"),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          },
+          onLongPress: () {},
+          child: Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.symmetric(vertical: 10),
+            // height: 50,
+            // width: MediaQuery.of(context).size.width * .6,
+            decoration: BoxDecoration(
+              color: pColor,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  textAlign: TextAlign.end,
+                  msg,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
